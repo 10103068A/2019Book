b17e55670761e799340f6472e36b4ab9ce2dd5de
Piagekuo <a13245550@gmail.com>
2019-12-06 22:10:17 +0800

    ch10

---
+++ TCP_ShootGame/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace TCP_ShootGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Q = new System.Windows.Forms.PictureBox();
+            this.P = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Q)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Q);
+            this.panel1.Controls.Add(this.P);
+            this.panel1.Location = new System.Drawing.Point(23, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(571, 552);
+            this.panel1.TabIndex = 0;
+            // 
+            // Q
+            // 
+            this.Q.Image = global::TCP_ShootGame.Properties.Resources.SQ;
+            this.Q.Location = new System.Drawing.Point(192, 10);
+            this.Q.Name = "Q";
+            this.Q.Size = new System.Drawing.Size(133, 101);
+            this.Q.TabIndex = 1;
+            this.Q.TabStop = false;
+            this.Q.Tag = "Q";
+            // 
+            // P
+            // 
+            this.P.Image = global::TCP_ShootGame.Properties.Resources.SP;
+            this.P.Location = new System.Drawing.Point(195, 440);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(130, 98);
+            this.P.TabIndex = 0;
+            this.P.TabStop = false;
+            this.P.Tag = "P";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(617, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 30);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "登入伺服器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(617, 289);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 184);
+            this.listBox1.TabIndex = 34;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox4.Location = new System.Drawing.Point(617, 511);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 25);
+            this.textBox4.TabIndex = 33;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox3.Location = new System.Drawing.Point(618, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 25);
+            this.textBox3.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(617, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 25);
+            this.textBox2.TabIndex = 31;
+            this.textBox2.Text = "2019";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(617, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 25);
+            this.textBox1.TabIndex = 30;
+            this.textBox1.Text = "192.168.1.118";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(613, 487);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 21);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "系統訊息";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(613, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "線上使用者";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(614, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 21);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "玩家名稱";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(613, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "伺服器Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(614, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "伺服器IP";
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Score.Location = new System.Drawing.Point(616, 551);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(20, 21);
+            this.Score.TabIndex = 36;
+            this.Score.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 25;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(778, 560);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 23);
+            this.button2.TabIndex = 37;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 586);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Q)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.P)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox P;
+        private System.Windows.Forms.PictureBox Q;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TCP_ShootGame/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCP_ShootGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Select();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Z:
+                    P.Left -= 5;
+                    break;
+                case Keys.X:
+                    P.Left += 5;
+                    break;
+                case Keys.Space:
+                    myShot();
+                    break;
+            }
+            if (listBox1.SelectedIndex >= 0)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Z:
+                    case Keys.X:
+                        Send("3" + P.Left.ToString() + "|" + listBox1.SelectedItem);
+                        break;
+                    case Keys.Space:
+                        Send("4S|" + listBox1.SelectedItem);
+                        break;
+                }
+            }
+            button2.Select();
+        }
+
+        private void myShot()
+        {
+            Label B = new Label();
+            B.Tag = "B";
+            B.Width = 3;
+            B.Height = 6;
+            B.BackColor = Color.Red;
+            B.Left = P.Left + P.Width / 2 - B.Width / 2;
+            B.Top = P.Top - B.Height;
+            panel1.Controls.Add(B);
+        }
+        private void XShot()
+        {
+            Label B = new Label();
+            B.Tag = "X";
+            B.Width = 3;
+            B.Height = 6;
+            B.BackColor = Color.Gray;
+            B.Left = Q.Left + Q.Width / 2 - B.Width / 2;
+            B.Top = Q.Bottom;
+            panel1.Controls.Add(B);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (Control c in panel1.Controls)
+            {
+                string s = c.Tag.ToString();
+                switch (s)
+                {
+                    case "B":
+                        c.Top -= 5;
+                        if (c.Bottom < 0) c.Dispose();
+                        if (chkHit((Label)c, Q))
+                        {
+                            c.Dispose();
+                            Score.Text = (int.Parse(Score.Text) + 1).ToString();
+                        }
+                        break;
+                    case "X":
+                        c.Top += 5;
+                        if (c.Top >panel1.Height) c.Dispose();
+                        break;
+                }
+            }
+        }
+        private bool chkHit(Label B, PictureBox C)
+        {
+            if (B.Right < C.Left) return false;
+            if (B.Left > C.Right) return false;
+            if (B.Bottom < C.Top) return false;
+            if (B.Top > C.Bottom) return false;
+            return true;
+        }
+
+        Socket T;
+        Thread Th;
+        string User;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button2.Select();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            User = textBox3.Text;
+            string IP = textBox1.Text;
+            int Port = int.Parse(textBox2.Text);
+
+            try
+            {
+                IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port);
+                T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                T.Connect(EP);
+                Th = new Thread(Listen);
+                Th.IsBackground = true;
+                Th.Start();
+                textBox4.Text = "已連接伺服器\r\n";
+                Send("0" + User);
+                button1.Enabled = false;
+            }
+            catch (Exception)
+            {
+                textBox4.Text = "無法連上伺服器\r\n";
+            }
+        }
+
+        private void Send(string Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            T.Send(B, 0, B.Length, SocketFlags.None);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (button1.Enabled == false)
+            {
+                Send("9" + User);
+                T.Close();
+            }
+        }
+
+        bool Xbang;
+        private void Listen()
+        {
+            EndPoint ServerEP = (EndPoint)T.RemoteEndPoint;
+            byte[] B = new byte[1023];
+            int inLen = 0;
+            string Msg;
+            string St;
+            string Str;
+            while (true)
+            {
+                try
+                {
+                    inLen = T.ReceiveFrom(B, ref ServerEP);
+                }
+                catch (Exception)
+                {
+                    T.Close();
+                    listBox1.Items.Clear();
+                    MessageBox.Show("伺服器斷了！");
+                    button1.Enabled = true;
+                    Th.Abort();
+                }
+                Msg = Encoding.Default.GetString(B, 0, inLen);
+                St = Msg.Substring(0, 1);
+                Str = Msg.Substring(1);
+                switch (St)
+                {
+                    case "L":
+                        listBox1.Items.Clear();
+                        string[] M = Str.Split(',');
+                        listBox1.Items.AddRange(M);
+                        break;
+                    case "3":
+                        Q.Left = panel1.Width - int.Parse(Str) - Q.Width;
+                        break;
+                    case "4":
+                        Xbang = true;
+                        break;
+                }
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button2.Select();
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (Xbang)
+            {
+                XShot();
+                Xbang = false;
+            }
+        }
+    }
+}
